0d32aa3765cbb675a430056635d0467673f6323b
dashbah <dsbakhankova@edu.hse.ru>
2023-05-28 22:10:02 +0300

    initial commit

---
+++ Restoran_auth/src/main/java/restoran/RestoranAuthApplication.java
@@ -0,0 +1,13 @@
+package restoran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoranAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestoranAuthApplication.class, args);
+    }
+
+}
